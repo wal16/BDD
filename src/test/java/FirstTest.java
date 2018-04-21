90431af61da29a3e53ae0aebf9b0f0db61e7f8fc
Waldemar Mozoła <waldemar.mozola@onet.pl>
2018-04-21 17:02:44 +0200

    Add cucumber

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,20 @@
+import cucumber.api.CucumberOptions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/cucumber",
+                "json:target/result.json",
+                "junit:target/result.xml"
+        }
+)
+
+
+
+
+public class FirstTest {
+}
---
+++ src/test/resources/new.feature
@@ -0,0 +1,8 @@
+Feature: Math subtraction
+
+  Scenario: Can subtraction two numbers
+    Given number is 2
+    And number is 1
+    And number is 1
+    When I  subtraction numbers
+    Then I get 0 in result
